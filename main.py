506113c80a150e09bf9759d935c00b38b07bcc72
stautuan <stautuan89@gmail.com>
2024-07-22 16:18:46 -0400

    read the contents of the book

---
+++ main.py
@@ -0,0 +1,13 @@
+def main():
+    book_path = "books/frankenstein"
+    text = get_book_text(book_path)
+    print(text)
+
+
+def get_book_text(path_to_file):
+    with open(path_to_file) as f:
+        file_content = f.read()
+        return file_content
+
+
+main()
